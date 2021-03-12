cc07f6efe91aa9eb0dc9c5edffd8ae9d6e07425f
Yi-Jing <meg778899@gmail.com>
2021-03-12 16:59:14 +0800

    新增購物清單的總價、單價/數量正整數的判斷

--- all.js
+++ all.js
@@ -6,12 +6,13 @@ var app = new Vue({
             price: 0,
             num: 0
         }],
-        display: []
+        display: [],
+
     },
     computed: {
         result: function(){
             var sum=0;
-            this.product.forEach(i => {
+            this.display.forEach(i => {
                 sum+=i.price*i.num;
             });
             return sum
@@ -19,7 +20,18 @@ var app = new Vue({
     },
     methods: {
         insertList: function(products){
-            this.display.push({name: products.name, price: products.price, num: products.num})
+            if(products.name==null || products.price==null || products.num==null){
+                alert("請輸入完整資料");
+            }
+            else if (products.price<0 || products.num<0){
+                alert("單價或數量請輸入正整數");
+            }
+            else if(products.num%1!=0){
+                alert("數量請輸入正整數");
+            }
+            else{
+                this.display.push({name: products.name, price: products.price, num: products.num})
+            }
         },
         deleteList: function(products){
             this.display.splice(this.display.indexOf(products), 1);       
